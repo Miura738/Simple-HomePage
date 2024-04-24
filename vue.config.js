5cab1eba746710ee3d12592d662edf15e743a88c
AoMiura <miura738@1oli.cn>
2024-04-24 09:22:46 +0800

    debug

--- vue.config.js
+++ vue.config.js
@@ -8,13 +8,13 @@ module.exports = defineConfig({
     pages: {
         // 先配置主页
         index: {
-            entry: './src/main.js',
+            entry: './src/main.ts',
             template: './public/index.html',
             title: 'AoMiura\'s Home'
         },
         // 再配置各个子页面：登录后课表查询页
         notfound: {
-            entry: './src/main.js',
+            entry: './src/main.ts',
             template: './public/404.html',
             title: 'AoMiura\'s Home'
         }
