d61d2678f88734f471e7de87ccc229a13ad4c1ab
AoMiura <miura738@1oli.cn>
2024-04-24 09:18:27 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,19 @@ module.exports = defineConfig({
   productionSourceMap: false,
   transpileDependencies: true,
   assetsDir: 'static',
-  lintOnSave: false
+  lintOnSave: false,
+    pages: {
+        // 先配置主页
+        index: {
+            entry: './src/main.js',
+            template: './public/index.html',
+            title: 'AoMiura\'s Home'
+        },
+        // 再配置各个子页面：登录后课表查询页
+        notfound: {
+            entry: './src/main.js',
+            template: './public/404.html',
+            title: 'AoMiura\'s Home'
+        }
+    }
 })
